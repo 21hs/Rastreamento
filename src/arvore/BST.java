b96e83e7290dd6856d52ec8036660f88c38b1640
Anderson Ferraz <usehxx@gmail.com>
2024-11-22 20:23:37 -0300

    Add files via upload

---
+++ src/arvore/BST.java
@@ -0,0 +1,20 @@
+class BST extends BinaryTree {
+
+    boolean search(int key) {
+        return searchRec(root, key);
+    }
+
+    boolean searchRec(Node root, int key) {
+        if (root == null) {
+            return false;
+        }
+        if (key == root.data) {
+            return true;
+        }
+        if (key < root.data) {
+            return searchRec(root.left, key);
+        } else {
+            return searchRec(root.right, key);
+        }
+    }
+}
---
+++ src/arvore/BinaryTree.java
@@ -0,0 +1,140 @@
+class BinaryTree {
+    class Node {
+        int data;
+        Node left, right;
+
+        Node(int item) {
+            data = item;
+            left = right = null;
+        }
+    }
+
+    Node root;
+
+    BinaryTree() {
+        root = null;
+    }
+
+    void insert(int data) {
+        root = insertRec(root, data);
+    }
+
+    Node insertRec(Node root, int data) {
+        if (root == null) {
+            root = new Node(data);
+            return root;
+        }
+        if (data < root.data) {
+            root.left = insertRec(root.left, data);
+        } else if (data > root.data) {
+            root.right = insertRec(root.right, data);
+        }
+        return root;
+    }
+
+    void inorder() {
+        inorderRec(root);
+    }
+
+    void inorderRec(Node root) {
+        if (root != null) {
+            inorderRec(root.left);
+            System.out.print(root.data + " ");
+            inorderRec(root.right);
+        }
+    }
+
+    int height() {
+        return heightRec(root);
+    }
+
+    int heightRec(Node root) {
+        if (root == null) {
+            return 0;
+        }
+        int leftHeight = heightRec(root.left);
+        int rightHeight = heightRec(root.right);
+        return Math.max(leftHeight, rightHeight) + 1;
+    }
+
+    int countLeaves() {
+        return countLeavesRec(root);
+    }
+
+    int countLeavesRec(Node root) {
+        if (root == null) {
+            return 0;
+        }
+        if (root.left == null && root.right == null) {
+            return 1;
+        }
+        return countLeavesRec(root.left) + countLeavesRec(root.right);
+    }
+
+    boolean isBalanced() {
+        return isBalancedRec(root) != -1;
+    }
+
+    int isBalancedRec(Node root) {
+        if (root == null) {
+            return 0;
+        }
+        int leftHeight = isBalancedRec(root.left);
+        int rightHeight = isBalancedRec(root.right);
+
+        if (leftHeight == -1 || rightHeight == -1 || Math.abs(leftHeight - rightHeight) > 1) {
+            return -1;
+        }
+        return Math.max(leftHeight, rightHeight) + 1;
+    }
+
+    boolean isBST() {
+        return isBSTRec(root, Integer.MIN_VALUE, Integer.MAX_VALUE);
+    }
+
+    boolean isBSTRec(Node node, int min, int max) {
+        if (node == null) {
+            return true;
+        }
+        if (node.data < min || node.data > max) {
+            return false;
+        }
+        return isBSTRec(node.left, min, node.data - 1) && isBSTRec(node.right, node.data + 1, max);
+    }
+
+    void delete(int data) {
+        root = deleteRec(root, data);
+    }
+
+    Node deleteRec(Node root, int data) {
+        if (root == null) {
+            return root;
+        }
+
+        if (data < root.data) {
+            root.left = deleteRec(root.left, data);
+        } else if (data > root.data) {
+            root.right = deleteRec(root.right, data);
+        } else {
+            if (root.left == null) {
+                return root.right;
+            } else if (root.right == null) {
+                return root.left;
+            }
+
+            root.data = minValue(root.right);
+
+            root.right = deleteRec(root.right, root.data);
+        }
+        return root;
+    }
+
+    int minValue(Node root) {
+        int minValue = root.data;
+        while (root.left != null) {
+            minValue = root.left.data;
+            root = root.left;
+        }
+        return minValue;
+    }
+}
